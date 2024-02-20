3d64219af2f94c7fd4c4098473fafeb526617e7f
Jayden-747 <jaydenhutchinson747@gmail.com>
2024-02-19 19:42:49 -0800

    sync

--- COMP_1537/Coding/Assignment_4/public/js/client.js
+++ COMP_1537/Coding/Assignment_4/public/js/client.js
@@ -1,1 +1,14 @@
+async function listChange() {
+  // Fetch the content of the HTML file containing the new list
+  const response = await fetch("/app/html/newlist.html");
+  const data = await response.text();
+
+  // Get the original list elemetn
+  const originalList = document.getElementById("placeholder");
+
+  originalList.innerHTML = data;
+}
+
+console.log(listChange());
+
 console.log("client.js loaded.");
